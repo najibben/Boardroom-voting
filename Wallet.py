e08ec0bf8420ae4558b447122f1ebdc502d3400b
najibben <ben.haddou.perez.najib@gmail.com>
2021-11-26 13:48:05 +0100

    fix signature

--- Wallet.py
+++ Wallet.py
@@ -1,14 +1,19 @@
-from Crypto.PublicKey import RSA  # Generates public and private keys
-from Crypto.Signature import PKCS1_v1_5  # Special algorithm that generate signatures
+
 from Crypto.Hash import SHA256
-import Crypto.Random  # Generates Random number
+from Crypto.Hash import SHA3_256
 import binascii  # Converts binary to ascii and the other way around
 from Crypto.PublicKey import ECC
 from Crypto.Signature import DSS
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import utils
+
+from cryptography import exceptions
 import binascii
 import sys
+
 
 class Wallet:
     def __init__(self, node_id):
@@ -20,6 +25,9 @@ class Wallet:
         private_key, public_key = self.generate_keys()
         self.private_key = private_key
         self.public_key = public_key
+ 
+      
+   
 
     def save_keys(self):
         if self.public_key is not None and self.private_key is not None:
@@ -48,19 +56,8 @@ class Wallet:
 
     def generate_keys(self):
         
-        private_key = ec.generate_private_key(ec.SECP384R1())
-        if (len(sys.argv)>1):
-           type=int(sys.argv[1])
-
-        if (type==1): private_key = ec.generate_private_key(ec.SECP192R1())
-        elif (type==2): private_key = ec.generate_private_key(ec.SECP224R1())
-        elif (type==3): private_key = ec.generate_private_key(ec.SECP256K1())
-        elif (type==4): private_key = ec.generate_private_key(ec.SECP256R1())
-        elif (type==5): private_key = ec.generate_private_key(ec.SECP384R1())
-        elif (type==6): private_key = ec.generate_private_key(ec.SECP521R1())
-        elif (type==7): private_key = ec.generate_private_key(ec.BrainpoolP256R1())
-        elif (type==8): private_key = ec.generate_private_key(ec.BrainpoolP384R1())
-        elif (type==9): private_key = ec.generate_private_key(ec.BrainpoolP512R1())
+        private_key = ec.generate_private_key(ec.SECP256R1())
+       
         vals = private_key.private_numbers()
         no_bits=vals.private_value.bit_length()
         print (f"Private key value: {vals.private_value}. Number of bits {no_bits}")
@@ -80,22 +77,30 @@ class Wallet:
         #private_key = ECC.generate(curve='P-256')
         #private_key = ec.generate_private_key(ec.SECP256K1())
         #return (binascii.hexlify(private_key.export_key(format='DER')).decode('ascii'), binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
-        private_key = der
-        public_key = der1
-        return (binascii.b2a_hex(private_key.export_key(format='DER')).decode(), binascii.b2a_hex(public_key.export_key(format='DER')).decode())
+        private_key = pem
+        public_key = pem1
+        return ((private_key).decode(), (public_key).decode())
+        #return (binascii.b2a_hex(private_key.export_key(format='DER')).decode('ascii'), binascii.b2a_hex(public_key.export_key(format='DER')).decode('ascii'))
+
 
     def sign_transaction(self, sender, recipient, amount):
-        h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
-        #signature = signer.sign(h)
+        
+        h = SHA3_256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
         signature = DSS.new(ECC.import_key(self.private_key), 'fips-186-3').sign(h)
-        return binascii.hexlify(signature).decode('ascii')
+        #return binascii.hexlify(signature).decode('ascii')
+        print('\nsignature: {}\n'.format(signature.hex()))
+        #return signature.hex()
+        print(binascii.hexlify(signature).decode())
+        return binascii.hexlify(signature).decode()
+       
 
     @staticmethod
     def verify_transaction(transaction):
         # If is MINING sender, we don't have to validate, because MINING does't have a valid signature.
-        public_key = ECC.import_key(binascii.unhexlify(transaction.sender))
+        public_key = ECC.import_key(transaction.sender)
         verifier = DSS.new(public_key, 'fips-186-3')
-        h = SHA256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
-        return verifier.verify(h, binascii.unhexlify(transaction.signature))
-
-  
+        h = SHA3_256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
+        #return (verifier.verify(h, binascii.unhexlify(transaction.signature)))
+        return (binascii.unhexlify(transaction.signature))
+      
+     
